833609513f0de0d555b5ee2502bc63a6cfb81868
Ledger <dr-jermy@outlook.com>
2026-05-08 01:01:12 +0300

    DSA training

---
+++ Algorithms/linear_search.py
@@ -0,0 +1,19 @@
+def linear_search(list, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+        else:
+            return None
+
+
+def verify(index):
+    if index is not None:
+        print("Target found in position: ", index)
+    else:
+        print("Target is not in the list")
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+results = linear_search(numbers, 1)
+verify(results)
